c0245e2b57390e61398c19e04ec24e8815a2b7d0
CristinaDale01 <cristina.200182@gmail.com>
2025-11-03 19:59:44 +0100

    inserimento componente dropdown

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -23,81 +23,10 @@ function Navbar() {
       <img src="/logo.png" alt="logo" className="logo" />
 
       {/* VOCI DI MENU */}
-      <ul className="nav-list">
-        <li className="nav-item">
-          <button
-            className={activeMenu === "destinazioni" ? "active" : ""}
-            onClick={() => handleMenuClick("destinazioni")}
-          >
-            Destinazioni
-          </button>
-          {openMenu === "destinazioni" && (
-            <ul className="dropdown">
-              <li>Islanda</li>
-              <li>Stati Uniti</li>
-              <li>Africa</li>
-              <li>Francia</li>
-              <li>Spagna</li>
-              <li>Portogallo</li>
-              <li>Croazia</li>
-              <li>Grecia</li>
-            </ul>
-          )}
-        </li>
-      <li className="nav-item">
-        <button
-          className={activeMenu === "modo" ? "active" : ""}
-          onClick={() => handleMenuClick("modo")}
-        >
-          Modo di viaggiare
-        </button>
-          {openMenu === "modo" && (
-            <ul className="dropdown">
-              <li>Van</li>
-              <li>Camper</li>
-              <li>Moto</li>
-              <li>Macchina</li>
-              <li>Treno</li>
-              <li>A piedi</li>
-            </ul>
-          )}
-      </li>
-
-      <li className="nav-item">
-          <button
-            className={activeMenu === "tipo" ? "active" : ""}
-            onClick={() => handleMenuClick("tipo")}
-          >
-            Tipo di vacanza
-          </button>
-          {openMenu === "tipo" && (
-            <ul className="dropdown">
-              <li>Avventura</li>
-              <li>Relax</li>
-              <li>Cultura</li>
-            </ul>
-          )}
-      </li>
-
-        <li className="nav-item">
-          <button
-            className={activeMenu === "stagione" ? "active" : ""}
-            onClick={() => handleMenuClick("stagione")}
-          >
-            Viaggia per stagione
-          </button>
-          {openMenu === "stagione" && (
-            <ul className="dropdown">
-              <li>Inverno</li>
-              <li>Estate</li>
-              <li>Primavera</li>
-              <li>Autunno</li>
-            </ul>
-          )}
-        </li>
-      </ul>
-      <Navdropdown items={[ "testo1", "testo2", "testo3" ]} text={"nome dropdown"} />
-      <Navdropdown items={[ "testo4", "testo5", "testo6" ]} text={"nome dropdown 2"} />
+      <Navdropdown items={[ "Islanda", "Stati Uniti", "Africa", "Francia", "Spagna", "Portogallo", "Croazia", "Grecia"]} text={"Destinazioni"} />
+      <Navdropdown items={[ "Van", "Camper", "Moto", "Macchina", "Treno", "A piedi" ]} text={"Modo di viaggiare"} />
+      <Navdropdown items={[ "Avventura", "Relax", "Cultura" ]} text={"Tipo di vacanza"} />
+      <Navdropdown items={[ "Autunno", "Inverno", "Primavera", "Estate" ]} text={"Viaggia per stagione"} />
       {/* ICONA CUORE DESTRA */}
       <span className="icon-heart">♡</span>
     </nav>
